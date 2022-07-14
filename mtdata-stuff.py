b0219c2b2781a27ccde472bd0a64bf55af1dc9f0
XapaJIaMnu <nheart@gmail.com>
2022-07-14 15:04:21 +0100

    mtdata downloader

--- mtdata-stuff.py
+++ mtdata-stuff.py
@@ -1,4 +1,11 @@
-from typing import Optional, Iterable
+#!/usr/bin/env python3
+"""Various mtdata dataset downloading utilities"""
+import os
+from typing import Optional, Iterable, Dict, List
+from concurrent.futures import ThreadPoolExecutor
+from subprocess import check_call, CalledProcessError
+from sys import stderr
+from collections import defaultdict
 from pydantic import BaseModel
 from fastapi import FastAPI
 from mtdata.entry import lang_pair
@@ -40,9 +47,42 @@ def list_datasets(langs:str) -> Iterable[Entry]:
             group = entry.did.group,
             name = entry.did.name,
             version = entry.did.version,
-            langs = entry.did.langs
+            langs = [lang.lang for lang in entry.did.langs]
         ) for entry in get_entries(lang_pair(langs))
     )
+
+def dedupe_datasests(datasets: Iterable[Entry]) -> Iterable[Entry]:
+    """Mtdata contains a multitude a datasets that have many different versions
+    (eg europarl). In the vast majority of the cases we ONLY EVER want the latest version"""
+    datadict: Dict[str, List[Entry]] = defaultdict(list)
+    for entry in datasets:
+        datadict[entry.name].append(entry)
+    # Sort by version and return one per name
+    return ([sorted(entrylist, key=lambda t: t.version, reverse=True)[0] for entrylist in datadict.values()])
+
+def get_dataset(entry: Entry, path: str) -> None:
+    """Gets datasets, using a subprocess call to mtdata. Might be less brittle to internal
+    mtdata interface changes"""
+    call_type: str # We have train/test/dev
+    if "dev" in entry.name:
+        call_type = "-dv"
+    elif "test" in entry.name:
+        call_type = "-ts"
+    else:
+        call_type = "-tr"
+    # Download the dataset
+    try:
+        check_call(["mtdata", "get", "-l", "-".join(entry.langs), call_type, entry.id, "--compress",  "-o", path])
+    except CalledProcessError as err:
+        print("Error downloading dataset:", entry.id, file=stderr)
+        print(err.cmd, file=stderr)
+        print(err.stderr, file=stderr)
+
+def get_datasets(datasets: Iterable[Entry], path: str, num_threads: int=2) -> None:
+    """Gets multiple datasets with up to num_theads parallel downloads"""
+    executor = ThreadPoolExecutor(max_workers=num_threads)
+    for entry in datasets:
+        executor.submit(get_dataset(entry, path))
 
 
 @app.get("/datasets/{did}")
@@ -53,3 +93,10 @@ def read_dataset(did: str):
 @app.get("/datasets/{did}/sample")
 def read_dataset(did: str):
     return datasets[did].did
+
+def test() -> None:
+    """Tests downloading all eng-bul datasests with a threadpool of subprocess calls"""
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    datasets = dedupe_datasests(list_datasets("eng-bul"))
+    get_datasets(datasets, 'data', 4)

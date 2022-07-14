1f6eee2b592b98a99f1fabc08115cab528936735
XapaJIaMnu <nheart@gmail.com>
2022-07-14 13:06:19 +0100

    Convert tabs to spaces and make it a bit more pythonic

--- datasets.py
+++ datasets.py
@@ -1,26 +1,31 @@
+#!/usr/bin/env python3
+"""Lists datasets given a directory. It works by scanning the directory and looking for gz files."""
 import os
 from itertools import groupby
 
 def list_datasets(path) -> dict[str,dict[str,os.DirEntry]]:
-	with os.scandir(path) as entries:
-		files = [
-			entry
-			for entry in entries
-			if entry.is_file() 
-			and entry.name.endswith('.gz')
-			and not entry.name.startswith('.')
-		]
+    """Lists datasets given a directory. Scans the directories and returns a dictionary of the
+    datasets encoutered. Dictionary looks like {dataset_name : { lang: path}}"""
+    with os.scandir(path) as entries:
+        files = [
+            entry
+            for entry in entries
+            if entry.is_file()
+            and entry.name.endswith('.gz')
+            and not entry.name.startswith('.')
+        ]
 
-	return {
-		name: {
-			entry.name.rsplit('.', 2)[1]: entry
-			for entry in files
-		}
-		for name, files in groupby(
-			sorted(files, key=lambda entry: entry.name),
-			key=lambda entry: entry.name.rsplit('.', 2)[0])
-	}
+    return {
+        name: {
+            entry.name.rsplit('.', 2)[1]: entry
+            for entry in files
+        }
+        for name, files in groupby(
+            sorted(files, key=lambda entry: entry.name),
+            key=lambda entry: entry.name.rsplit('.', 2)[0])
+    }
 
 if __name__ == '__main__':
-	import sys, pprint
-	pprint.pprint(list_datasets(sys.argv[1]))
+    import sys
+    import pprint
+    pprint.pprint(list_datasets(sys.argv[1]))

210df4a1d921b97d670d8b4ce5bebff49db19237
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-08-12 16:20:44 +0100

    Show languages per dataset

--- static/dataset-editor.js
+++ static/dataset-editor.js
@@ -5,10 +5,17 @@ export default {
 		datasets: Array
 	},
 
+	methods: {
+		getLanguages(dataset) {
+			return Object.keys(dataset.columns)
+		}
+	},
+
 	template: `
 		<table>
 			<tr v-for="dataset in datasets">
 				<td>{{ dataset.name }}</td>
+				<td>{{ getLanguages(dataset).join(', ') }}</td>
 				<td><router-link :to="{name: 'filter-editor', params: {datasetName: dataset.name}}">Filters</router-link></td>
 			</tr>
 		</table>

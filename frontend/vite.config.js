cdb7016a00255d547621e71ac639f6a7086ed761
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-09-07 12:04:48 +0200

    Host app on /frontend/

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/frontend/',
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8000/'
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

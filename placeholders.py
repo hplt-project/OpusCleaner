a39cd5943221862daf7d57553bdb038d34e74518
Saipade <81774545+Saipade@users.noreply.github.com>
2022-09-09 16:48:24 +0200

    fix bugs

--- placeholders.py
+++ placeholders.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 from typing import List
 import yaml
@@ -5,7 +6,6 @@ from dataclasses import dataclass
 import re
 import argparse
 import random
-import json
 import sentencepiece as spm
 
 placeholders, sp = {}, None
@@ -13,13 +13,15 @@ placeholders, sp = {}, None
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', type=str, help='Path to yaml configuration file', required=True)
 parser.add_argument('-s', '--source_file', type=str, help='Path to the input file')
-parser.add_argument('-t', '--target_file', type=str, help='Path to the target file', required=True)
-parser.add_argument('--dump_placeholders', action='store_true', help='Check to print placeholders out')
 parser.add_argument('-v', '--vocab', type=str, help='Path to the vocab file')
 parser.add_argument('-n', '--num_placeholders', type=int, default=20, help='Maximum number of placeholders')
 mutex_group_1 = parser.add_mutually_exclusive_group(required=True)
 mutex_group_1.add_argument('--decode', action='store_true')
 mutex_group_1.add_argument('--encode', action='store_true')
+mutex_group_2 = parser.add_mutually_exclusive_group()
+mutex_group_2.add_argument('-t', '--target_file', type=str, help='Path to the target file')
+mutex_group_2.add_argument('--dump_placeholders', action='store_true', help='Check to print placeholders out') 
+
 
 @dataclass
 class Rule(object):
@@ -49,7 +51,7 @@ class Text(str):
         # use regex rules
         for rule in rules:
             for grp in [match.group() for match in re.finditer(rule.pattern, self)]:
-                if grp not in placeholders:
+                if grp not in [placeholder for placeholder in placeholders.values()]:
                     # generate new unique number for placeholder
                     new_number = generate_random_in_range()
                     placeholders[new_number] = grp
@@ -73,11 +75,9 @@ class Text(str):
 
                 if piece == "<unk>":
                     # generate new unique number for placeholder
-                    new_number = generate_random_in_range()
+                    new_number = generate_random_in_range() if token not in placeholders.values() \
+                            else get_key_from_placeholders(token)
                     placeholders[new_number] = token
-                    self = re.sub(token, f'@{new_number}', self)
-                elif token in placeholders:
-                    new_number = get_key_from_placeholders(token)
                     self = re.sub(token, f'@{new_number}', self)
         return self
 
@@ -89,6 +89,11 @@ class Text(str):
         return self
 
 
+def dump_placeholders():
+    [print(f'@{placeholder}', end=' ') for placeholder in placeholders.keys()]
+    print()
+
+
 def get_src() -> List[str]:
     if not args.source_file:
         return [line for line in sys.stdin]
@@ -98,30 +103,39 @@ def get_src() -> List[str]:
 
 
 def encode() -> None:
-    with open(args.target_file, 'w') as target_file, \
+    with open(args.target_file, 'w') if args.target_file else sys.stdout as target_file, \
          open(args.config, 'r') as config_file:
 
         config, text = yaml.safe_load(config_file), get_src()
         rules = [Rule(regex) for regex in config['regexes']]
         [target_file.write(Text(line).make_placeholders(*rules)) for line in text]
         config["placeholders"], config["num-placeholders"] = placeholders, len(placeholders)
+
     with open(args.config, 'w') as config_file:
         yaml.dump(config, config_file, allow_unicode=True)
-    print(json.dumps(placeholders, indent=4)) if args.dump_placeholders else None
 
 
 def decode() -> None:
-    with open(args.config, 'r') as config_file, \
-         open(args.target_file, 'w') as target_file:
+    with open(args.target_file, 'w') if args.target_file else sys.stdout as target_file, \
+         open(args.config, 'r') as config_file:
         
         text = get_src()
         placeholders = yaml.safe_load(config_file)['placeholders']
         [target_file.write(Text(line).replace_placeholders(placeholders)) for line in text]
-    print(json.dumps(placeholders, indent=4)) if args.dump_placeholders else None
-        
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
     if args.vocab:
         sp = spm.SentencePieceProcessor(args.vocab)
+
+    if args.dump_placeholders:
+        with open(args.config, 'r') as config_file:
+            placeholders = yaml.safe_load(config_file)['placeholders']
+            dump_placeholders()
+        exit()
+
+    if not args.target_file:
+        print(f'No target file provided for {sys.argv[0]}, output will be directed to stdout', file=sys.stderr)
     encode() if args.encode else decode()

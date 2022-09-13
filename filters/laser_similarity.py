ad2b4127dbd6c4cb99f80ec7a9b2e8a65a3b235d
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-09-13 10:36:24 +0200

    Attempt to use the scorer/threshold wrapper with the LASER filter

--- filters/laser_similarity.py
+++ filters/laser_similarity.py
@@ -23,22 +23,32 @@ def _cosine_sim(emb1: np.ndarray, emb2: np.ndarray) -> np.ndarray:
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                      description="Filter a parallel dataset using LASER.")
-    parser.add_argument("--threshold", type=float, default=0.5, help="Minimum accepted LASER score.")
     parser.add_argument("--batch-size", type=int, default=32, help="LASER batch size")
     parser.add_argument("--src-lang", type=str, required=True, help="Two-letter source language code (ISO 639-1)")
     parser.add_argument("--tgt-lang", type=str, required=True, help="Two-letter target language code (ISO 639-1)")
+    
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--threshold", type=float, help="Minimum accepted LASER score.")
+    group.add_argument("--scores", action="store_true", help="Print scores instead of lines")
 
     args = parser.parse_args()
-    
+
+    if not args.scores and args.threshold is None:
+        print("Either use --threshold or --scores")
+
     laser = Laser()
 
     for batch in chunked(sys.stdin, args.batch_size):
         # TODO error checking of column count?
         scores = _compute_similarity(laser, [line.split("\t") for line in batch], args.src_lang, args.tgt_lang)
 
-        for line, score in zip(batch, scores):
-            if score >= args.threshold:
-                sys.stdout.write(line)
+        if args.scores:
+            for score in scores:
+                print(score, file=sys.stdout)
+        else:
+            for line, score in zip(batch, scores):
+                if score >= args.threshold:
+                    sys.stdout.write(line)
 
 
 if __name__ == "__main__":

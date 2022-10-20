1665aea45527313448cfc5c7832b6a37237a3e05
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-10-20 14:01:10 +0100

    Sort filters & readonly

--- frontend/src/store/filters.js
+++ frontend/src/store/filters.js
@@ -1,4 +1,4 @@
-import {ref} from 'vue';
+import {ref, readonly} from 'vue';
 
 const filters = ref([]);
 
@@ -7,14 +7,18 @@ async function fetchFilters() {
 	const filterDict = await response.json();
 
 	// Turn the {name:Filter} map into a [Filter] list and fold the 'name' attribute into the Filter.name property.
-	return Array.from(Object.entries(filterDict), ([name, value]) => Object.assign(value, {name}))
+	const filters = Array.from(Object.entries(filterDict), ([name, value]) => Object.assign(value, {name}))
+
+	return filters.sort((a, b) => a.name.localeCompare(b.name));
 }
 
 let filterRequest = null;
 
 export function getFilters() {
 	if (!filterRequest)
-		filterRequest = fetchFilters().then(data => filters.value.splice(0, filters.length, ...data));
+		filterRequest = fetchFilters().then(data => {
+			filters.value.splice(0, filters.length, ...readonly(data))
+		});
 
-	return filters.value;
+	return filters;
 }

45dcdb93037c41e5f97dc6375c053460fb672d8a
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-12-02 12:16:59 +0000

    Remove random.sh! Why? Click here to find out!
    Because it has been superseded by shuffle.py

--- trainer/random.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# $1 = seed, $2 = outputfile, $3 = inputfile(s)
-SEED="$1"
-shift
-
-OUTFILE="$1"
-shift
-
-cat "$@" | shuf --random-source=<(openssl enc -aes-256-ctr -pass pass:"$SEED" -nosalt </dev/zero 2>/dev/null) -o "$OUTFILE"

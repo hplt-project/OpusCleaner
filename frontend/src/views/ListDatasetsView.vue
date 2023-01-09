39099ad209d901491341de5581398849e88e308f
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-01-09 15:22:00 +0000

    Datasets table

--- frontend/src/views/ListDatasetsView.vue
+++ frontend/src/views/ListDatasetsView.vue
@@ -5,9 +5,12 @@ import { getDatasets } from '../store/datasets.js';
 import { getFilterSteps } from '../store/filtersteps.js';
 import { getCategoriesForDataset } from '../store/categories.js';
 import CategoryPicker from '../components/CategoryPicker.vue';
+import {UploadIcon, CodeIcon, FilterIcon, PieChartIcon, Edit3Icon, TagIcon} from 'vue3-feather';
 
 function languages(dataset) {
-	return Object.keys(dataset?.columns || {});
+	const keys = Object.keys(dataset?.columns || {});
+	const intl = new Intl.DisplayNames([], {type:'language'});
+	return keys.map(lang => intl.of(lang));
 }
 
 const categoryPicker = ref(); // Element
@@ -15,53 +18,160 @@ const categoryPicker = ref(); // Element
 </script>
 
 <template>
-	<div class="dataset-list">
-		<table>
+	<div class="table-container-first-screen">
+		<Teleport to=".navbar">
+			<RouterLink class="import-data-button" v-bind:to="{name:'add-dataset'}">
+				Import dataset
+				<UploadIcon/>
+			</RouterLink>
+		</Teleport>
+
+		<h2 class="table-title">Your datasets</h2>
+
+		<table class="datasets-table">
 			<thead>
 				<tr>
 					<th>Name</th>
 					<th>Languages</th>
 					<th>Categories</th>
 					<th>Filter steps</th>
+					<th>Actions</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr v-for="dataset in getDatasets()" :key="dataset.id">
 					<td>{{ dataset.name }}</td>
-					<td>{{ languages(dataset).join(', ') }}</td>
-					<td>
-						<button @click="event => categoryPicker.showForDataset(dataset, event)">Edit</button>
-						<span class="category" v-for="category in getCategoriesForDataset(dataset)" :key="category.name">{{ category.name }}</span>
+					<td>{{ languages(dataset).join('–') }}</td>
+					<td class="tags">
+						<div class="tags-container">
+							<div class="category-tags">
+								<span class="tag" v-for="category in getCategoriesForDataset(dataset)" :key="category.name">
+									<TagIcon/>
+									<span class="tag-name">{{ category.name }}</span>
+								</span>
+							</div>
+							<button class="icon-button" @click="event => categoryPicker.showForDataset(dataset, event)"><Edit3Icon/></button>
+						</div>
 					</td>
-					<td><router-link :to="{name: 'edit-filters', params: {datasetName: dataset.name}}">Filters ({{ getFilterSteps(dataset).value.length }})</router-link></td>
+					<td class="filter-steps">
+						{{ getFilterSteps(dataset).value.length }}
+					</td>
+					<td>
+						<RouterLink class="icon-button" :to="{}"><CodeIcon/></RouterLink>
+						<RouterLink class="icon-button" :to="{name: 'edit-filters', params: {datasetName: dataset.name}}"><FilterIcon/></RouterLink>
+						<RouterLink class="icon-button" :to="{}"><PieChartIcon/></RouterLink>
+					</td>
 				</tr>
 			</tbody>
-			<tfoot>
-				<tr>
-					<td colspan="3">
-						<RouterLink v-bind:to="{name:'add-dataset'}">Download dataset…</RouterLink>
-					</td>
-				</tr>
-			</tfoot>
 		</table>
 		<CategoryPicker ref="categoryPicker"></CategoryPicker>
 	</div>
 </template>
 
-<style>
+<style scoped>
 @import '../css/categories.css';
 
-.dataset-list {
-	flex: 1;
-	overflow: auto;
+.icon-button {
+	appearance: none;
+	border: none;
+	background: inherit;
+	cursor: pointer;
+	color: inherit;
 }
 
-.dataset-list table {
-	width: 100%;
+.icon-button:visited {
+	color: inherit;
 }
 
-.dataset-list table tr:nth-child(2n) td {
-	background: rgba(128,128,128, 0.25);
+.import-data-button {
+  display: flex;
+  align-items: center;
+  border: none;
+  border-radius: 3px;
+  height: 40px;
+  padding: 0 30px;
+  background-color: #17223d;
+  color: #e4960e;
+  font-size: 18px;
+}
+
+.import-data-icon {
+  margin-left: 5px;
+}
+.datasets-table {
+  border: 2px solid #1c3948;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.datasets-table thead {
+  background-color: #17223d;
+  color: #e4960e;
+  font-size: 20px;
+  text-align: left;
+}
+
+.datasets-table tbody {
+  color: #17223d;
+}
+
+.datasets-table thead th {
+  padding: 20px 0px 10px 10px;
+  min-width: 150px;
+}
+
+.datasets-table tbody tr {
+  border-bottom: 1px solid #1c3948;
+  border-right: 2px solid #1c3948;
+}
+
+.datasets-table tbody td {
+  border-right: 2px solid #1c3948;
+}
+
+.datasets-table tbody tr td {
+  padding: 10px;
+}
+
+.category-tags {
+  display: flex;
+}
+
+.tags-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.tags {
+  width: 45%;
+  font-family: "Fira Code", monospace;
+}
+
+.filter-steps {
+  text-align: right;
+}
+
+.tag {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  padding: 3px 20px 3px 10px;
+  border-radius: 15px;
+  margin-right: 5px;
+  font-size: 14px;
+}
+
+.green-tag {
+  background-color: #afffca;
+}
+
+.red-tag {
+  background-color: #ffafaf;
+}
+
+.blue-tag {
+  background-color: #afcfff;
 }
 
 </style>

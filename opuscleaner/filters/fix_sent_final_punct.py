a9d4c11043d86e48570984488f181b607d6e2d2d
Nikolay Bogoychev <nheart@gmail.com>
2023-08-08 15:15:44 +0000

    use sets and  proper condition aroudn french quotes

--- opuscleaner/filters/fix_sent_final_punct.py
+++ opuscleaner/filters/fix_sent_final_punct.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-my_punct = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '»', '«', '“', '”']
+my_punct = {'!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '»', '«', '“', '”'}
 
 for line in sys.stdin:
     src, trg = line.rstrip("\r\n").split("\t")
@@ -16,9 +16,9 @@ for line in sys.stdin:
         trg = trg[:-2] + trg[-1]
 
     # check for the french quotes special case
-    if src[-1] == '»' or src[-1] == '«' and trg[-1] not in my_punct:
+    if (src[-1] == '»' or src[-1] == '«') and trg[-1] not in my_punct:
         trg = trg + '"'
-    elif trg[-1] == '»' or trg[-1] == '«' and src[-1] not in my_punct:
+    elif (trg[-1] == '»' or trg[-1] == '«') and src[-1] not in my_punct:
         src = src + '"'
     elif src[-1] in my_punct and trg[-1] not in my_punct:
         trg = trg + src[-1]

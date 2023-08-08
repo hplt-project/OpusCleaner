61f4671eee9c529a705b3c766dbbf9f2dc7b4451
Nikolay Bogoychev <nheart@gmail.com>
2023-08-08 17:11:05 +0000

    More fixes towards swapped punctuation

--- opuscleaner/filters/fix_sent_final_punct.py
+++ opuscleaner/filters/fix_sent_final_punct.py
@@ -17,6 +17,12 @@ for line in sys.stdin:
     # Sometimes two punctuation marks are swapped...
     if len(src) >=2 and len(trg) >= 2 and src[-2] == trg[-1] and src[-1] == trg[-2]:
        trg = trg[:-2] + src[-2] + src[-1]
+    # Sometimes they are swapped with space around eg SPACE». -> .SPACE»
+    if len(src) >=3 and src[-1] in my_punct and src[-2] == '»' and src[-3] == ' ':
+       src = src[:-3] + src[-1] + ' ' + src[-2]
+    if len(trg) >=3 and trg[-1] in my_punct and trg[-2] == '»' and trg[-3] == ' ':
+       trg = trg[:-3] + trg[-1] + ' ' + trg[-2]
+
 
     # check for the french quotes special case
     if (src[-1] == '»' or src[-1] == '«') and trg[-1] not in my_punct:

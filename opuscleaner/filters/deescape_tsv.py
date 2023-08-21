4c727fd4fc91797be3da6ee4e3d2354a0d792247
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-08-21 10:57:34 +0100

    Fix `deescape_tsv` from throwing when it encounters an empty field
    Re #106.

--- opuscleaner/filters/deescape_tsv.py
+++ opuscleaner/filters/deescape_tsv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import sys
 
-QUOTECHR = b'"'[0]
+QUOTECHR = ord('"')
 
 for line in sys.stdin.buffer:
 	fields = line.rstrip(b"\r\n").split(b"\t")
 	for i, field in enumerate(fields):
-		if field[0] == QUOTECHR and field[-1] == QUOTECHR:
+		if len(field) > 0 and field[0] == QUOTECHR and field[-1] == QUOTECHR:
 			fields[i] = field[1:-1].replace(b'""', b'"')
 	sys.stdout.buffer.write(b"\t".join(fields))
 	sys.stdout.buffer.write(b"\n")

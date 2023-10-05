268bf046337f282f8901abffa607f7e6f80dda7a
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-10-05 10:07:09 +0100

    Expand and document the expression used in num_mismatch

--- opuscleaner/filters/num_mismatch.py
+++ opuscleaner/filters/num_mismatch.py
@@ -5,8 +5,21 @@ import sys
 from typing import Match, TextIO
 
 
+NUM_EXPR = re.compile(r"""
+	(?:
+		(?P<sign>(?:(?<=\s)|^)[-+])  # start with a sign if it is not attached to a word, i.e. not - in tid-30
+		|\b                          # or start with a word boundary, i.e. not just30
+	)
+	(?:0*)                         # allow for 0 prefixes which we then ignore when comparing
+	(?P<value>\d+                  # digits before the first comma or dot
+		(?:[\.,]\d+)*                # allow commas or dots, i.e. 300,000.0 but not 3,,,5
+	)
+	\b                             # disallow 30beep, but also 30th and 1st #TODO
+""", re.X)
+
+
 def normalize(numstr:Match) -> str:
-	return numstr['sign'] + re.sub(r'[^\d]+', '*', numstr['value']) # ignore the decimal and digit separators
+	return (numstr['sign'] or '') + re.sub(r'[^\d]+', '*', numstr['value']) # ignore the decimal and digit separators
 
 
 def filter_numerical_mismatch(fin: TextIO, fout: TextIO, ratio: float, *, debug: bool = False):
@@ -15,7 +28,7 @@ def filter_numerical_mismatch(fin: TextIO, fout: TextIO, ratio: float, *, debug:
 
 		assert len(cols) >= 2
 
-		nums_left, nums_right = (set(map(normalize, re.finditer(r'(?P<sign>[-+]?)(?:0*)(?P<value>\d+(?:[\.,]\d+)*)', col))) for col in cols[:2])
+		nums_left, nums_right = (set(map(normalize, re.finditer(NUM_EXPR, col))) for col in cols[:2])
 
 		# Only bother calculating the ratio if there were any numbers to begin with
 		if nums_left and nums_right:
